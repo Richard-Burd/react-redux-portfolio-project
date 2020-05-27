625753d7737c8ed85a0fe236eeab8d7231245ca4
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-05-26 18:38:56 -0700

    dynamic form onChange being worked on

--- client/src/components/ParameterForm.js
+++ client/src/components/ParameterForm.js
@@ -1,36 +1,47 @@
-import React from 'react'
+// This is close to what I want to do here, but with the extra step of breaking
+// down an array of objects.  Here we just want to break down a single object.
+// https://www.storyblok.com/tp/react-dynamic-component-from-json
+
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import { updateAttitude } from '../redux'
 
-function ParameterForm(props) {
-  // these may not get used...I dunno, but if they do get used...
-  // implement this logic here:
-  // https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react
-  // https://github.com/jaketrent/demo-single-change-handler/blob/master/src/App.js
-  /*
-  const [state, setState] = React.useState({
-    hooks: true
-  })
+const RenderChildren = (props) => { // <= this needs to be a class component
 
-  function handleChange(event) {
-    const value =
-      event.target.type === "checkbox" ? event.target.checked : event.target.value;
-    setState({
-      ...state,
-      [event.target.name]: value
-    });
+  // https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
+  const [value, setValue] = useState(props.data);
+
+  const handleClick = (event) => {
+    //const value = event.target.value
   }
-  */
 
+  const children = []
+  for (const key in props.data) {
+    //console.log(`We've got a key of: ${key} and a value of: ${props.data[key]}`);
+    //console.log(Object.keys(props.data).length);
+    children.push(
+      React.createElement('div', {key: children.length ,id: "container"}, // key is the unique key
+        React.createElement('label', {id: "label"}, key),
+        React.createElement('input', {
+          type: "text",
+          value: props.data[key],
+          onChange: event => {setValue(event.target.value)}
+        })
+      )
+    )
+
+  }
+  return <div>{children}</div>
+}
+
+function ParameterForm(props) {
   return (
-
     <div>
       <div>This is the Parameter Form Page for {props.type} Params No. {props.info.id}</div>
-      {/*
-        the logic is as follows:
-        iterate over each parameter { props.info } and display
-        an <input> field and a <label> for each one.
-      */}
+      <div>We have {(Object.keys(props.info).length)} variables to render</div>
+      <form className={"form"}>
+        <RenderChildren data={props.info} />
+      </form>
     </div>
   )
 }
